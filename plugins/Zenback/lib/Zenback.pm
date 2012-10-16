85b414030ecdc77fc080c43ae2ff78f4a9ace189
WebbingStudio <mail@webbingstudio.com>
2012-10-17 01:26:33 +0900

    最新のコードに対応、ダイナミック・パブリッシングでbase_uriの参照が間違っているのを修正

--- plugins/Zenback/lib/Zenback.pm
+++ plugins/Zenback/lib/Zenback.pm
@@ -15,7 +15,7 @@ sub _hdlr_zenback_tag {
     my $entry     = $ctx->stash('entry');
     my $url       = uri_escape($blog->site_url);
 
-    my $button ='<!-- BEGIN ZenBackWidget --><script type="text/javascript">document.write(unescape("%3Cscript src=\'http://widget.zenback.jp/?motouri="+encodeURIComponent(document.referrer)+"&base_uri=' . $url . '&nsid=' . $config->{'zenback_key'} . '&rand="+Math.ceil((new Date()*1)*Math.random())+"\' type=\'text/javascript\'%3E%3C/script%3E"));</script><!-- END ZenBackWidget -->';
+    my $button ='<!-- X:S ZenBackWidget --><script type="text/javascript">document.write(unescape("%3Cscript src=\'http://widget.zenback.jp/?base_uri=' . $url . '&nsid=' . $config->{'zenback_key'} . '&rand="+Math.ceil((new Date()*1)*Math.random())+"\' type=\'text/javascript\'%3E%3C/script%3E"));</script><!-- X:E ZenBackWidget -->';
 
 #     open  FH, ">> debug.log";
 #     print FH "Debugging \n $button \n";
